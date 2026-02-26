0a0be1b1572c8005322215d63e02e8fbfeed4b49
Your Name <sblolroblox@gmail.com>
2026-02-26 18:50:36 -0500

    chore: Update gospa dependency to v0.1.4 and refresh generated routes.

--- cli/output.go
+++ cli/output.go
@@ -178,6 +178,6 @@ func PrintBanner() {
      /_/             	
 `
 	fmt.Println(printer.colorize(cyan, banner))
-	fmt.Printf("%s %s\n", printer.Dim("A modern SPA framework for Go"), printer.Dim("v0.1.3"))
+	fmt.Printf("%s %s\n", printer.Dim("A modern SPA framework for Go"), printer.Dim("v0.1.4"))
 	fmt.Println()
 }
