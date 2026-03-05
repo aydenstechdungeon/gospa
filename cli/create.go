01d62a438bf627958f2b3b26a0415cf27796c50a
Your Name <>
2026-03-04 19:49:19 -0500

    jgfhy

--- cli/create.go
+++ cli/create.go
@@ -230,7 +230,6 @@ templ Layout(title string) {
 		<meta name="description" content="A GoSPA application"/>
 		<title>{ title }</title>
 		<link rel="stylesheet" href="/static/css/style.css"/>
-		@gospatempl.RuntimeScript("/_gospa/runtime.js")
 	</head>
 	<body>
 		<main>
