4f5accb3a0ee7b2909c21429c1b0c815f8e11798
Your Name <>
2026-03-06 05:02:11 -0500

    chore: bump version to v0.1.19

--- gospa.go
+++ gospa.go
@@ -39,7 +39,7 @@ type StateSerializerFunc func(interface{}) ([]byte, error)
 type StateDeserializerFunc func([]byte, interface{}) error
 
 // Version is the current version of GoSPA.
-const Version = "0.1.18"
+const Version = "0.1.19"
 
 // Config holds the application configuration.
 type Config struct {
