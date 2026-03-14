a026df6a8a3bbbfd3a3f27d062e0da1f5859dc75
Your Name <sblolroblox@gmail.com>
2026-03-14 14:43:45 -0400

    chore: bump version to v0.1.28

--- gospa.go
+++ gospa.go
@@ -41,7 +41,7 @@ type StateSerializerFunc func(interface{}) ([]byte, error)
 type StateDeserializerFunc func([]byte, interface{}) error
 
 // Version is the current version of GoSPA.
-const Version = "0.1.27"
+const Version = "0.1.28"
 
 // Serialization formats
 const (
