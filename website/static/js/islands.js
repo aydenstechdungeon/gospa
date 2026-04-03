3614d0ddd7f0d2210121af3aef56297d871de832
Your Name <sblolroblox@gmail.com>
2026-04-03 19:27:08 -0400

    refactor: replace lazy dynamic imports with direct setup registration for island components

--- website/static/js/islands.js
+++ website/static/js/islands.js
@@ -1,17 +1,20 @@
-import {
-  __require,
-  __toESM
-} from "./islands-3hqyeswk.js";
+// generated/routesdocsgospasfcpage.ts
+function __gospa_setup_routesdocsgospasfcpage(element, { props, state }) {
+  console.log("Gospa SFC Documentation loaded");
+  console.log("Hello from TypeScript!");
+  const __GOSPA_HANDLERS__ = {};
+  window["__GOSPA_ISLAND_" + "routesdocsgospasfcpage" + "__"] = { handlers: __GOSPA_HANDLERS__ };
+  const scope = (selector) => element.querySelector(selector + "." + "gospa-9d0f");
+}
+function mount(element, props, state) {
+  __gospa_setup_routesdocsgospasfcpage(element, { props, state });
+}
 
 // generated/islands.ts
-function registerLazySetup(name, loader) {
+function registerSetup(name, setup) {
   window.__GOSPA_SETUPS__ = window.__GOSPA_SETUPS__ || {};
-  window.__GOSPA_SETUPS__[name] = async (el, props, state) => {
-    const mod = await loader();
-    const hydrateFn = mod.hydrate || mod.default?.hydrate || mod.mount || mod.default?.mount;
-    if (hydrateFn) {
-      return hydrateFn(el, props, state);
-    }
-  };
+  window.__GOSPA_SETUPS__[name] = setup;
 }
-registerLazySetup("routesdocsgospasfcpage", () => import("./routesdocsgospasfcpage-aj2av0zx.js"));
+registerSetup("routesdocsgospasfcpage", (el, props, state) => {
+  mount(el, props, state);
+});
